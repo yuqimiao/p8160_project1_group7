f76c484707bb708f1cd20509cc2e1d341f8d6407
Jiayi Shen <shen@Jiayis-MacBook-Pro.local>
2020-02-10 12:31:54 -0500

    change sigma

--- R_script/simulation8.R
+++ R_script/simulation8.R
@@ -6,6 +6,6 @@ beta_results <- list()
 fitted_results <- list()
 sample_size = 1000
   temp_results <- simulation(time = 100, n = sample_size, p = 200, percentage = c(5, 3, 2), 
-                             sigma = 0.5, type = "definite")
+                             sigma = 1, type = "definite")
 saveRDS(temp_results, file = paste("result_missing_weak_n", sample_size, ".rds", sep = ""))
 
